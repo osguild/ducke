46db74ee16cc88cc68999e99ff2bd25c2c6d0dab
Brandon Ly <brandonly@lostcoding.com>
2025-12-10 19:33:42 -0600

    add ratatui (#12)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use crossterm::event::{self, Event};
+use ratatui::{Frame, text::Text};
+
 fn main() {
-    println!("Hello, world!");
+    let mut terminal = ratatui::init();
+    loop {
+        terminal.draw(draw).expect("failed to draw frame");
+
+        if matches!(event::read().expect("failed to read event"), Event::Key(_)) {
+            break;
+        }
+    }
+    ratatui::restore();
+}
+
+fn draw(frame: &mut Frame) {
+    let text = Text::raw("Hello, World!");
+    frame.render_widget(text, frame.area());
 }
